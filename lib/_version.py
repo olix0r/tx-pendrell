feff0d785f4e9a7f5983f063c5b35bf5b8b46516
Oliver V. Gould <github@olix0r.net>
2010-06-20 23:09:51 -0700

    Version bump

--- lib/_version.py
+++ lib/_version.py
@@ -2,7 +2,7 @@ from twisted.python.versions import Version
 
 author = "Oliver V. Gould <pendrell-devel@olix0r.net>"
 copyright = """Copyright (c) 2008-2010 Oliver V. Gould.  All rights reserved."""
-version = Version("pendrell", 0, 1, 0)
+version = Version("pendrell", 0, 2, 0)
 
 # Don't export t.p.v.Version
 del Version
