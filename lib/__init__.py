1b57a2d571b9607171771221f2d7001d6836c97b
Oliver V. Gould <github@olix0r.net>
2010-07-12 17:37:05 -0700

    Fix breakage from _version/setup cleanup.  Add conveniences for "from pendrell import Agent" etc.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,13 +1,12 @@
-# Copyright (c) 2008-2010 liver Gould.
+# Copyright (c) 2008-2010 Oliver Gould.
 # All rights reserved.
 
 """
 Pendrell: A Twisted HTTP/1.1 User Agent for the Programmatic Web.
 """
 
-from pendrell._version import author, copyright, version
+from pendrell._version import copyright, version
+from pendrell.agent import Agent, getPage, downloadPage
 
-__author__ = "Oliver Gould <pendrell@olix0r.net>"
-__id__ = "$Id: $"[5:-2]
 __copyright__ = copyright
 __version__ = version.short()
